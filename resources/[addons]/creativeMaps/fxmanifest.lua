67c90187f0b2f70e32bca3b093b3f89aea1bf861
DuMonster2501 <mateusgalbiati2501@gmail.com>
2022-08-15 14:36:14 -0300

    Summary five

---
+++ resources/[addons]/creativeMaps/fxmanifest.lua
@@ -0,0 +1,69 @@
+fx_version "bodacious"
+game "gta5"
+
+this_is_a_map "yes"
+
+files {
+	"stream/*",
+	"stream/*/**",
+	"stream/*/*/**",
+	"stream/*/*/*/**",
+	"interiorproxies.meta"
+}
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/v_int_16.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/v_int_49.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/creative_hotel.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/creative_middle.ytyp"
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/**.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/escada_rds_03.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/props_favelas.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/props_zn.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/pilarluskas1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/rualuskas.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/rualuskas2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_03.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_04.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_05.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_06.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_07.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_08.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_09.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_10.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_11.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_12.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/graffitiluskas_13.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/rualuskas3.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/rualuskas4.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/rua_luskas05.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/propsz3zin.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/escadanova.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/favela.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/stt_mp_stunt_hoardings.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/escada2a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/caveirao.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/casas.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/props_rds.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/favela_brkk_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/cercaluskas1a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/cercaluskas1b.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/apoiocaixaluskas.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/escadaluskas1a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/escadaluskas1b.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/barraco_17luskas.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/barraco_18luskas.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/barracoluskas.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/barracoluskas_01a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/barracoluskas_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/cercaluskas_01a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/cercaluskas_01b.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/ponteluskas_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/barracoluskas_02a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/barracoluskas_02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/barracoluskas_03.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/creativeBarragem/ytyp/ferrocercaluskas1.ytyp'
