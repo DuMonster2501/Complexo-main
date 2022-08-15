52fac0ba03a1b768dc85966e4e80c1bd1b0ec37f
DuMonster2501 <mateusgalbiati2501@gmail.com>
2022-08-15 14:51:45 -0300

    Summary six

---
+++ resources/[addons]/creativeVehs/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version "bodacious"
+game "gta5"
+
+client_script "names.lua"
+
+files {
+	"data/**/*.meta",
+	"audio/*",
+	"audio/**/*"
+}
+
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"
+data_file "FIVEM_LOVES_YOU_341B23A2F0E0F131" "data/**/popgroups.ymt"
+
+data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
+data_file "AUDIO_GAMEDATA" "audio/elegyx/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/elegyx/elegyx_sounds.dat"
---
+++ resources/[addons]/igro45_digitalden/__resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+Author 'Igro45'
+
+Discord_Mapping 'https://discord.gg/2bjt66Z43r'
+
+Twitch 'https://twitch.tv/igro45'
+
+YouTube 'https://www.youtube.com/channel/UCv-Bl3W4KJOwXnJKBqG8gTA'
+
+Discord_Tag '✪igro45#0045'
